5736e6395f0978c60adb21b3def7247941d734d9
isis52300 <isis.dumas000@gmail.com>
2020-01-30 15:43:54 -0800

    Edited add.hpp

---
+++ add.hpp
@@ -0,0 +1,40 @@
+#ifndef __ADD_HPP__
+#define __ADD_HPP__
+
+#include "base.hpp"
+
+class Add : public Base {
+public:
+    Add(double num1, double num2) : Base() {
+        object1 = new Op(num1);
+        object2 = new Op(num2);
+    }
+    
+    Add(double num1, Base obj1) : Base() {
+        object1 = new Op(num1);
+        object2 = obj1;
+    }
+    
+    Add(Base obj1, double num1) : Base() {
+        object1 = obj1;
+        object2 = new Op(num1);
+    }
+    
+    Add(Base obj1, Base obj2) : Base() {
+        object1 = obj1;
+        object2 = obj2;
+    }
+    
+    virtual double evaluate() {
+        return object1.evaluate() + object2.evaluate();
+    }
+    
+    virtual std::string stringify() { return object1.stringify() + '+' + object2.stringify(); }
+    
+private:
+    Base object1;
+    Base object2;
+    string function;
+};
+
+#endif //__ADD_HPP__
